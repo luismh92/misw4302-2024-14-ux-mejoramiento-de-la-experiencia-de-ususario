f47f7837bcafcde710e34a023c71f0151f3eee0a
luis.mendoza <l.mendozah@uniandes.edu.co>
2024-09-23 16:59:30 -0500

    adding modal

--- web-alarma/src/app/app.component.ts
+++ web-alarma/src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatListModule} from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatListModule, MatIconModule, RouterLink],
+  imports: [RouterOutlet, MatListModule, MatIconModule, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'web-alarma';
+
+
+  isOpen = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.relative')) {
+      this.isOpen = false;
+    }
+  }
 }
